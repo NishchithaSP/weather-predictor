9d2b23be7899e6a2c97cbcbf5447d996fcc96967
NishchithaSP <102474289+NishchithaSP@users.noreply.github.com>
2023-09-27 02:14:02 +0530

    Includes date and day

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const weatherButton = document.getElementById('weather-button');
     const loadingSpinner = document.getElementById('loading-spinner');
     const result = document.getElementById('result');
+    const dateDayElement = document.getElementById('date-day'); // Get the date-day element
+
+    // Function to get the current date and day
+    function getCurrentDateAndDay() {
+        const currentDate = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        return currentDate.toLocaleDateString('en-US', options);
+    }
+
+    // Display the current date and day
+    dateDayElement.textContent = getCurrentDateAndDay();
 
     weatherButton.addEventListener('click', function () {
         weatherText.style.display = 'none';
